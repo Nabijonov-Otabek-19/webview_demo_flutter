a970df3baa6a205570e3998ae7b2e00d8c41ad4b
Otabek Nabijonov <nabijonovo23@gmail.com>
2024-03-18 14:21:36 +0300

    project updated

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -11,41 +11,98 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  final controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(Colors.white)
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {},
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith("https://www.youtube.com/")) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-        onWebResourceError: (WebResourceError error) {
-          print("WebView error = ${error.description}");
-        },
-      ),
-    );
+  var loadingPercentage = 0;
+  String url = "";
+  late WebViewController controller;
 
   @override
   void initState() {
     if (widget.url.startsWith('https://')) {
-      controller.loadRequest(Uri.parse(widget.url));
+      url = widget.url;
     } else {
-      controller.loadRequest(Uri.parse('https://${widget.url}'));
+      url = 'https://${widget.url}';
     }
+    controller = WebViewController()
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            setState(() {
+              loadingPercentage = progress;
+            });
+          },
+          onPageStarted: (String url) {
+            setState(() {
+              loadingPercentage = 0;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              loadingPercentage = 100;
+            });
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith("https://www.youtube.com/")) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+          onWebResourceError: (WebResourceError error) {
+            print("WebView error = ${error.description}");
+          },
+        ),
+      )
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..loadRequest(Uri.parse(url));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: WebViewWidget(controller: controller),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () async {
+              final messenger = ScaffoldMessenger.of(context);
+              if (await controller.canGoBack()) {
+                await controller.goBack();
+              } else {
+                messenger.showSnackBar(
+                  const SnackBar(content: Text('No back history item')),
+                );
+                return;
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () async {
+              final messenger = ScaffoldMessenger.of(context);
+              if (await controller.canGoForward()) {
+                await controller.goForward();
+              } else {
+                messenger.showSnackBar(
+                  const SnackBar(content: Text('No forward history item')),
+                );
+                return;
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.replay),
+            onPressed: () => controller.reload(),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: controller),
+          if (loadingPercentage < 100)
+            LinearProgressIndicator(value: loadingPercentage / 100.0),
+        ],
+      ),
     );
   }
 }
